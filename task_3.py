59961aa872e98242e43d827e990ed97791330271
JeckaSh <jeckashevelkov@icloud.com>
2026-02-28 14:28:31 +0300

    task 5 solution, replace task 3 solution

--- task_3.py
+++ task_3.py
@@ -10,10 +10,8 @@ for key, value in world_champions.items():
     print(key, '-', value)
 
 country = 'Италия'
-
-for key, value in world_champions.items():
-    if country in value:
-        print('Италия cтановилась чемпионом мира по футболу в 21 веке!')
-    else:
-        print('Италия не выигрывала чемпионат мира по футболу в 21 веке.')
-    
+ 
+if country in world_champions.values():
+    print('Италия cтановилась чемпионом мира по футболу в 21 веке!')
+else:
+    print('Италия не выигрывала чемпионат мира по футболу в 21 веке.')
